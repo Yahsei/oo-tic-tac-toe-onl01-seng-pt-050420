161aa71dab69fe4bee64cc1dc4c10a51a782148d
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-06-03 13:47:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,7 @@ class TicTacToe
       
       def turn 
         puts "Where would you like to move? (1-9)"
-        input = 
+        input = input_to_index(gets.chomp)
         input = input_to_index(input)
         if self.valid_move?(index)
           self.move(index, self.current_player)
