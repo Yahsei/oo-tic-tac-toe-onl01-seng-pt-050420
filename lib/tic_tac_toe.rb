bb8ec938a753826e71e73869478c768e71932408
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-06-03 13:39:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,7 +51,7 @@ class TicTacToe
         input = gets.strip
         input = input_to_index(input)
         if self.valid_move?(index)
-          self.move 
+          self.move(index, self.current_player)
        else
        end
 end
